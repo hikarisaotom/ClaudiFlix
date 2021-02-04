90bc00ecd6e90d7c2f33b4de1cab9e520d4293ec
Claudia Cortes <clau_cortes10@hotmail.com>
2021-02-03 22:51:33 -0600

    Getting favorites from one user; TODO: Implement views and validations

--- app/src/main/java/com/example/clauditter/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/clauditter/MovieDetailsActivity.kt
@@ -1,5 +1,5 @@
 package com.example.clauditter
-import android.app.Fragment
+
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -8,38 +8,49 @@ import androidx.activity.viewModels
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager.widget.ViewPager
 import com.bumptech.glide.Glide
+import com.example.clauditter.adapters.IS_LOGED
 import com.example.clauditter.adapters.PagerAdapter
 import com.example.clauditter.adapters.USERNAME
-import com.example.clauditter.adapters.IS_LOGED
 import com.example.clauditter.ui.clases.Movie
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.android.synthetic.main.movie_description.*
 
 
-private const val TAG="DETAILS"
-class MovieDetailsActivity : FragmentActivity(){
+private const val TAG = "DETAILS"
+
+class MovieDetailsActivity : FragmentActivity() {
     private lateinit var viewpager: ViewPager
     private val LogViewModel: LogViewModel by viewModels()
-
+    private var username: String? = ""
+    private var flag = false
+    private lateinit var movie: Movie
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.movie_description)
-        val movie=intent.getParcelableExtra<Movie>(MOVIE_TRANSFER) as Movie
+        movie = intent.getParcelableExtra<Movie>(MOVIE_TRANSFER) as Movie
         Glide.with(this)
             .load(movie.poster_path)
             .into(img_vistaPelicula)
         viewpager = findViewById(R.id.viewPagerDetails)
-        val pagerAdapter = PagerAdapter(supportFragmentManager,movie)
+        val pagerAdapter = PagerAdapter(supportFragmentManager, movie)
         viewpager.adapter = pagerAdapter
 
-        val username=intent.getStringExtra(USERNAME)
-        val flag=intent.getBooleanExtra(IS_LOGED,false)
+        username = intent.getStringExtra(USERNAME)
+        flag = intent.getBooleanExtra(IS_LOGED, false)
 
-        if(flag){
-            btn_addRemoveFavorite.visibility=View.VISIBLE
-        }else{
-            btn_addRemoveFavorite.visibility=View.GONE
+        if (flag) {
+            btn_addRemoveFavorite.visibility = View.VISIBLE
+        } else {
+            btn_addRemoveFavorite.visibility = View.GONE
             //todo verificar que este en la lista de los favoritos
         }
+        //PROJECT ID: toyproject-pp-fase1 TODO
+        btn_addRemoveFavorite.setOnClickListener(View.OnClickListener {
+            addRemoveFavorite()
+
+        })
 
 
     }
@@ -54,6 +65,51 @@ class MovieDetailsActivity : FragmentActivity(){
             viewpager.currentItem = viewpager.currentItem - 1
         }
     }
+
+
+    fun addRemoveFavorite() {
+        addToFavorites()
+       /* if (btn_addRemoveFavorite.text.equals(R.string.addFavorite)) {
+            btn_addRemoveFavorite.text = R.string.removeFavorite.toString()
+            btn_addRemoveFavorite.setTextColor(Color.RED)
+            addToFavorites()
+        } else {
+            btn_addRemoveFavorite.text = R.string.addFavorite.toString()
+            btn_addRemoveFavorite.setTextColor(Color.parseColor("#9E9E9E"))
+            //todo eliminar de la bdd de firebase
+        }
+        btn_addRemoveFavorite.setTextColor(Color.WHITE)*/
+    }
+
+    fun addToFavorites() {
+
+
+        val favorite = hashMapOf(
+            "username" to username,
+            "movieId" to movie.id,
+            "movieTitle" to movie.title
+        )
+        val db = FirebaseFirestore.getInstance()
+        db.collection("favorites")
+            .add(favorite)
+            .addOnSuccessListener {
+                Toast.makeText(
+                    this,
+                    "${movie.title} added to favorite",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(
+                    this,
+                    "There was an error :(",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+    }
+
+
 
 
 
--- app/src/main/java/com/example/clauditter/ui/Favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/clauditter/ui/Favorites/FavoritesFragment.kt
@@ -1,6 +1,7 @@
 package com.example.clauditter.ui.Favorites
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,18 +12,19 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.clauditter.LogViewModel
 import com.example.clauditter.R
+import com.google.firebase.firestore.FirebaseFirestore
 
 class FavoritesFragment : Fragment() {
 
     private lateinit var slideshowViewModel: FavoritesViewModel
     private val logInModel: LogViewModel by activityViewModels()
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         slideshowViewModel =
-                ViewModelProvider(this).get(FavoritesViewModel::class.java)
+            ViewModelProvider(this).get(FavoritesViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_favorites, container, false)
         val textView: TextView = root.findViewById(R.id.text_slideshow)
         slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
@@ -30,12 +32,32 @@ class FavoritesFragment : Fragment() {
         })
 
         logInModel.flag.observe(viewLifecycleOwner, Observer {
-            if(it){
-                textView.visibility=View.VISIBLE
-            }else{//Invisible: no s emuestra pero ocupa espacio en la pantalla, con gone se va todo
-                textView.visibility=View.GONE
+            if (it) {
+                textView.visibility = View.VISIBLE
+                getFavorites()
+            } else {//Invisible: no s emuestra pero ocupa espacio en la pantalla, con gone se va todo
+                textView.visibility = View.GONE
             }
         })
         return root
     }
-}
+
+
+    fun getFavorites() {
+        val db = FirebaseFirestore.getInstance()
+        db.collection("favorites")
+            .whereEqualTo("username", logInModel.user.value.toString())
+            .addSnapshotListener { snapshots, e ->
+                if (e != null) {
+                    //MOSTRAR NO HAY FAVORITOS POR AHORA
+                    Log.d("LOS USUARIO ", "NO HAY NADA QUE MOSTRAR")
+                    return@addSnapshotListener
+                }
+
+                snapshots?.documents?.forEach { document ->
+                    Log.d("LOS USUARIO ", "${document.data}"
+                    )
+                }
+            }
+    }
+}//End Of Class
